cbc67641ea9a812e78f4b9f19cd4268cf6a179a9
Craig Paul <craig.paul@coconutcalendar.com>
2019-07-26 09:44:22 -0600

    Adds mail message with tests to utilize new template api ability

---
+++ src/MailMessage.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Coconuts\Mail;
+
+use Illuminate\Notifications\Messages\MailMessage as Message;
+
+class MailMessage extends Message
+{
+    /**
+     * @var string
+     */
+    protected $alias;
+
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * Set the template alias.
+     *
+     * @param string $alias
+     *
+     * @return $this
+     */
+    public function alias($alias)
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    /**
+     * Get the data array for the mail message.
+     *
+     * @return array
+     */
+    public function data()
+    {
+        return [
+            'id' => $this->id,
+            'alias' => $this->alias,
+            'model' => $this->data,
+        ];
+    }
+
+    /**
+     * Set the template identifier.
+     *
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function identifier($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the data to be available within the template.
+     *
+     * @param array $data
+     *
+     * @return $this
+     */
+    public function include($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
