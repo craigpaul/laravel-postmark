6365c7d56bd8b2eed5e21de2605f6075585bd1e5
Mark van den Broek <mvdnbrk@gmail.com>
2019-07-27 11:49:48 +0200

    Adds double spaces to the doc blocks.

--- tests/PostmarkServiceProviderTest.php
+++ tests/PostmarkServiceProviderTest.php
@@ -17,8 +17,8 @@ class PostmarkServiceProviderTest extends TestCase
     /**
      * Get package providers.
      *
-     * @param \Illuminate\Foundation\Application $app
      *
+     * @param  \Illuminate\Foundation\Application  $app
      * @return array
      */
     protected function getPackageProviders($app)
