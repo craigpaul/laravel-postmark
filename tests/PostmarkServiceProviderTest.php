38d7a91ac370061504071446bcb6aa1ca498754f
Mark van den Broek <mvdnbrk@gmail.com>
2020-02-27 23:03:05 +0100

    Only run test for Laravel v6 and lower.

--- tests/PostmarkServiceProviderTest.php
+++ tests/PostmarkServiceProviderTest.php
@@ -7,13 +7,17 @@ use Coconuts\Mail\PostmarkServiceProvider;
 class PostmarkServiceProviderTest extends TestCase
 {
     /** @test */
-    public function will_skip_registering_transport_if_not_using_postmark()
+    public function will_skip_registering_transport_if_not_using_postmark_on_laravel_v6_and_lower()
     {
+        if (! $this->app->has('swift.transport')) {
+            $this->markTestSkipped('swift.transport is only available for Laravel 6.0 and lower.');
+        }
+
         $this->app['config']->set('mail.driver', 'array');
 
         (new PostmarkServiceProvider($this->app))->boot();
 
-        $this->assertEmpty($this->readProperty($this->app['mail.manager'], 'customCreators'));
+        $this->assertEmpty($this->readProperty($this->app['swift.transport'], 'customCreators'));
     }
 
     /**
