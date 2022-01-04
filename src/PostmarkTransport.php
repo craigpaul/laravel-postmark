d4b132c143b4f864ae2ee1082bcd0b7ce52048ea
Craig Paul <craig.paul@coconutsoftware.com>
2022-01-03 22:08:41 -0600

    Implements absolute basic code necessary to make a request to send an email via Postmark

--- src/PostmarkTransport.php
+++ src/PostmarkTransport.php
@@ -2,7 +2,32 @@
 
 namespace CraigPaul\Mail;
 
-class PostmarkTransport
+use Illuminate\Http\Client\Factory as Http;
+use Symfony\Component\Mime\RawMessage;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mailer\SentMessage;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+
+class PostmarkTransport implements TransportInterface
 {
-    //
+    public function __construct(
+        protected Http $http,
+    ) {
+    }
+
+    public function send(RawMessage $message, Envelope $envelope = null): ?SentMessage
+    {
+        $sentMessage = new SentMessage($message, $envelope ?? Envelope::create($message));
+
+        $response = $this->http->post('https://api.postmarkapp.com/email', []);
+
+        $sentMessage->setMessageId($response->json('MessageID'));
+
+        return $sentMessage;
+    }
+
+    public function __toString(): string
+    {
+        return 'postmark';
+    }
 }
