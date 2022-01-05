b7ff82ddfed75dae0c4c209ba539988018f3b28b
Craig Paul <craig.paul@coconutsoftware.com>
2022-01-05 17:23:56 -0600

    Adds tests to ensure using a templated mailable / mail message will successfully make the correct calls

--- tests/PostmarkTransportTest.php
+++ tests/PostmarkTransportTest.php
@@ -2,6 +2,11 @@
 
 namespace CraigPaul\Mail\Tests;
 
+use Illuminate\Support\Facades\Mail;
+use CraigPaul\Mail\TemplatedMailable;
+use CraigPaul\Mail\Tests\Factories\Template;
+use Illuminate\Support\Facades\Notification;
+use CraigPaul\Mail\Tests\Notifications\TemplatedNotification;
 use function basename;
 use CraigPaul\Mail\PostmarkTransport;
 use CraigPaul\Mail\PostmarkTransportException;
@@ -207,6 +212,53 @@ class PostmarkTransportTest extends TestCase
         $this->sendMessage($symfonyMessage);
     }
 
+    public function testCanSendTemplatedMailableSuccessfully()
+    {
+        $template = Template::create();
+
+        $factory = $this->fakeSuccessfulTemplate($template);
+
+        $mailable = (new TemplatedMailable())
+            ->alias($template->getAlias())
+            ->identifier($template->getId())
+            ->include($template->getModel());
+
+        Mail::to($template->getToAddress())->send($mailable);
+
+        $factory->assertSent(function (Request $request) use ($template) {
+            return $request->method() === 'POST'
+                && $request->isJson()
+                && $request->hasHeader('X-Postmark-Server-Token', $this->getToken())
+                && $request->url() === 'https://api.postmarkapp.com/email/withTemplate'
+                && $request['From'] === '"'.$template->getFromName().'" <'.$template->getFromAddress().'>'
+                && $request['To'] === $template->getToAddress()
+                && $request['TemplateId'] === $template->getId()
+                && $request['TemplateAlias'] === $template->getAlias()
+                && $request['TemplateModel'] === $template->getModel();
+        });
+    }
+
+    public function testCanSendTemplatedMailMessageSuccessfully()
+    {
+        $template = Template::create();
+
+        $factory = $this->fakeSuccessfulTemplate($template);
+
+        Notification::route('mail', $template->getToAddress())->notify(new TemplatedNotification($template));
+
+        $factory->assertSent(function (Request $request) use ($template) {
+            return $request->method() === 'POST'
+                && $request->isJson()
+                && $request->hasHeader('X-Postmark-Server-Token', $this->getToken())
+                && $request->url() === 'https://api.postmarkapp.com/email/withTemplate'
+                && $request['From'] === '"'.$template->getFromName().'" <'.$template->getFromAddress().'>'
+                && $request['To'] === $template->getToAddress()
+                && $request['TemplateId'] === $template->getId()
+                && $request['TemplateAlias'] === $template->getAlias()
+                && $request['TemplateModel'] === $template->getModel();
+        });
+    }
+
     protected function getMessageStreamId(): ?string
     {
         return $this->app['config']->get('mail.mailers.postmark.message_stream_id');
@@ -238,6 +290,23 @@ class PostmarkTransportTest extends TestCase
                 'To' => $email->getToAddress(),
                 'SubmittedAt' => Date::now()->format(DATE_RFC3339_EXTENDED),
                 'MessageID' => $email->getMessageId(),
+                'ErrorCode' => 0,
+                'Message' => 'OK',
+            ]),
+        ]);
+
+        $this->instance(Factory::class, $factory);
+
+        return $factory;
+    }
+
+    protected function fakeSuccessfulTemplate(Template $template): Factory
+    {
+        $factory = Http::fake([
+            'https://api.postmarkapp.com/email/withTemplate' => Http::response([
+                'To' => $template->getToAddress(),
+                'SubmittedAt' => Date::now()->format(DATE_RFC3339_EXTENDED),
+                'MessageID' => $template->getMessageId(),
                 'ErrorCode' => 0,
                 'Message' => 'OK',
             ]),
