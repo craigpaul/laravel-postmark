95e97fcaa36bd983ff883f09bc50aa57e5f7cddd
Craig Paul <craig.paul@coconutsoftware.com>
2022-01-04 22:29:26 -0600

    Updates existing test to ensure other required attribute is always sent

--- tests/Factories/Email.php
+++ tests/Factories/Email.php
@@ -4,38 +4,40 @@ namespace CraigPaul\Mail\Tests\Factories;
 
 use Faker\Factory;
 
-class EmailFactory
+class Email
 {
     public function __construct(
-        protected string $body,
         protected string $from,
+        protected string $htmlBody,
         protected string $messageId,
         protected string $subject,
+        protected string $textBody,
         protected string $to,
     ) {
     }
 
-    public static function create(): self
+    public static function createBasic(): self
     {
         $faker = Factory::create();
 
         return new self(
-            $faker->sentences(asText: true),
-            $faker->email(),
-            $faker->uuid(),
-            $faker->words(asText: true),
-            $faker->email(),
+            from: $faker->email(),
+            htmlBody: $faker->randomHtml(),
+            messageId: $faker->uuid(),
+            subject: $faker->words(asText: true),
+            textBody: $faker->sentences(asText: true),
+            to: $faker->email(),
         );
-    }
-
-    public function getBody(): string
-    {
-        return $this->body;
     }
 
     public function getFrom(): string
     {
         return $this->from;
+    }
+
+    public function getHtmlBody(): string
+    {
+        return $this->htmlBody;
     }
 
     public function getMessageId(): string
@@ -46,6 +48,11 @@ class EmailFactory
     public function getSubject(): string
     {
         return $this->subject;
+    }
+
+    public function getTextBody(): string
+    {
+        return $this->textBody;
     }
 
     public function getTo(): string
--- tests/PostmarkTransportTest.php
+++ tests/PostmarkTransportTest.php
@@ -3,7 +3,6 @@
 namespace CraigPaul\Mail\Tests;
 
 use Illuminate\Mail\Message;
-use Symfony\Component\Mime\Email;
 use Illuminate\Http\Client\Factory;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
@@ -11,19 +10,21 @@ use Illuminate\Support\Facades\Date;
 use CraigPaul\Mail\PostmarkTransport;
 use Symfony\Component\Mailer\Envelope;
 use Symfony\Component\Mailer\SentMessage;
-use CraigPaul\Mail\Tests\Factories\EmailFactory;
+use CraigPaul\Mail\Tests\Factories\Email;
+use Symfony\Component\Mime\Email as SymfonyEmail;
 
 class PostmarkTransportTest extends TestCase
 {
     public function testTransportSendsMessageSuccessfully()
     {
-        $email = $this->createFakeAttributes();
+        $email = Email::createBasic();
 
         $message = $this->newMessage()
             ->subject($email->getSubject())
             ->to($email->getTo())
             ->from($email->getFrom())
-            ->text($email->getBody());
+            ->html($email->getHtmlBody())
+            ->text($email->getTextBody());
 
         $symfonyMessage = $message->getSymfonyMessage();
 
@@ -50,31 +51,24 @@ class PostmarkTransportTest extends TestCase
                 && $request['From'] === $email->getFrom()
                 && $request['To'] === $email->getTo()
                 && $request['Subject'] === $email->getSubject()
-                && $request['TextBody'] === $email->getBody();
+                && $request['HtmlBody'] === $email->getHtmlBody()
+                && $request['TextBody'] === $email->getTextBody();
         });
-    }
-
-    protected function createFakeAttributes(): EmailFactory
-    {
-        return EmailFactory::create();
     }
 
     protected function newMessage(): Message
     {
-        return new Message(new Email());
+        return new Message(new SymfonyEmail());
     }
 
-    protected function sendMessage(Email $symfonyMessage): ?SentMessage
+    protected function sendMessage(SymfonyEmail $symfonyMessage): ?SentMessage
     {
         return $this->app->makeWith(PostmarkTransport::class, [
             'token' => $this->getToken(),
         ])->send($symfonyMessage, Envelope::create($symfonyMessage));
     }
 
-    /**
-     * @return mixed
-     */
-    protected function getToken()
+    protected function getToken(): string
     {
         return $this->app['config']->get('services.postmark.token');
     }
